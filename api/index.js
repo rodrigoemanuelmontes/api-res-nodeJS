e3624fc136fd18dcf9496e9ff6813c8e9ceb9755
Rodrigo Montes <rodrigomontes167@gmail.com>
2025-07-18 06:53:33 -0300

    realizo cambios

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
 import "dotenv/config"; 
-
 import express from 'express';
-import serverless from 'serverless-http'; 
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
@@ -20,17 +18,16 @@ app.use('/api/auth', authRoutes);
 
 // Ruta no encontrada (404)
 app.use((req, res, next) => {
-    const error = new Error('Ruta no encontrada');
-    error.status = 404;
-    next(error);
+  const error = new Error('Ruta no encontrada');
+  error.status = 404;
+  next(error);
 });
 
 // Manejo de errores generales
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    const status = err.status || 500;
-    res.status(status).json({ error: err.message || 'Error interno del servidor' });
+  console.error(err.stack);
+  const status = err.status || 500;
+  res.status(status).json({ error: err.message || 'Error interno del servidor' });
 });
 
-// NO usar app.listen() en Vercel, exportar handler para serverless
-export const handler = serverless(app);
+export default app;
